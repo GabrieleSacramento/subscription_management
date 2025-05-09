567ad3ba047e54460066aa34628eab495bfba043
Gabriele Sacramento <gabisacramentoti@gmail.com>
2025-05-09 18:33:29 -0300

    created splash screen

--- subscription_management/lib/src/modules/splash_screen/splash_screen.dart
+++ subscription_management/lib/src/modules/splash_screen/splash_screen.dart
@@ -2,11 +2,54 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage(name: 'SplashScreenRoute')
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.bounceOut);
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.white);
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(228, 228, 237, 1),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _animation,
+          builder: (context, child) {
+            return Transform.translate(
+              offset: Offset(0, 200 * (1 - _animation.value)),
+              child: child,
+            );
+          },
+          child: Image.asset('assets/images/subscription_management_logo.png'),
+        ),
+      ),
+    );
   }
 }
